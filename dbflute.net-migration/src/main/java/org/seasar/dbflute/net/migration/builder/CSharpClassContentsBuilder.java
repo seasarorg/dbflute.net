1de726ae8f65b0a282a5bd8bc40119dab27e9a00
jflute <dbflute@gmail.com>
2014-04-23 23:21:48 +0900

    SerialVersionUIDを削除するようにした

--- dbflute.net-migration/src/main/java/org/seasar/dbflute/net/migration/builder/CSharpClassContentsBuilder.java
+++ dbflute.net-migration/src/main/java/org/seasar/dbflute/net/migration/builder/CSharpClassContentsBuilder.java
@@ -1,10 +1,13 @@
 package org.seasar.dbflute.net.migration.builder;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 import org.seasar.dbflute.net.migration.analyzer.JavaInfo;
 import org.seasar.dbflute.util.DfCollectionUtil;
+import org.seasar.dbflute.util.Srl;
 
 /**
  * @author jflute
@@ -29,7 +32,9 @@ public class CSharpClassContentsBuilder extends CSharpBuilderBase {
     // ===================================================================================
     //                                                                           Attribute
     //                                                                           =========
+    protected String _previousLine;
     protected boolean _foundOverride;
+    protected boolean _removePrevious;
 
     // ===================================================================================
     //                                                                         Constructor
@@ -42,8 +47,11 @@ public class CSharpClassContentsBuilder extends CSharpBuilderBase {
     //                                                                      Class Contents
     //                                                                      ==============
     public void buildClassContents(StringBuilder sb) {
-        doBuildClassCommentClause(sb);
-        doBuildClassElementClause(sb);
+        StringBuilder contentsSb = new StringBuilder();
+        doBuildClassCommentClause(contentsSb);
+        doBuildClassElementClause(contentsSb);
+        String filtered = filterFinally(contentsSb);
+        sb.append(filtered);
     }
 
     protected void doBuildClassCommentClause(StringBuilder sb) {
@@ -53,11 +61,20 @@ public class CSharpClassContentsBuilder extends CSharpBuilderBase {
     }
 
     protected void doBuildClassElementClause(StringBuilder sb) {
+        final List<String> elementList = new ArrayList<String>();
         for (String line : _javaInfo.getClassElementList()) {
             final String classElement = convertClassElement(line);
-            if (classElement != null) {
-                sb.append(classElement).append(ln());
+            if (_removePrevious) {
+                elementList.remove(elementList.size() - 1);
+                _removePrevious = false;
             }
+            if (classElement != null) {
+                elementList.add(classElement);
+                _previousLine = classElement;
+            }
+        }
+        for (String element : elementList) {
+            sb.append(element).append(ln());
         }
     }
 
@@ -68,6 +85,11 @@ public class CSharpClassContentsBuilder extends CSharpBuilderBase {
         String work = line;
         if (isClassDefinitionLine(line)) {
             work = doConvertExtends(work);
+        } else if (isSerialVersionUIDLine(line)) {
+            if (_previousLine != null && _previousLine.trim().startsWith("/*")) {
+                _removePrevious = true;
+            }
+            return null;
         } else if (isOverrideAnnotationLine(line)) {
             _foundOverride = true;
             return null;
@@ -82,6 +104,9 @@ public class CSharpClassContentsBuilder extends CSharpBuilderBase {
         return doConvertBasicElement(work);
     }
 
+    // -----------------------------------------------------
+    //                                 Class Definition Line
+    //                                 ---------------------
     protected boolean isClassDefinitionLine(String line) {
         return line != null && line.startsWith("public ") && line.endsWith(" {");
     }
@@ -94,16 +119,32 @@ public class CSharpClassContentsBuilder extends CSharpBuilderBase {
         return replace(replace(work, "public ", "public override "), "protected ", "protected override ");
     }
 
+    // -----------------------------------------------------
+    //                               Serial Version UID Line
+    //                               -----------------------
+    protected boolean isSerialVersionUIDLine(String line) {
+        return line != null && line.contains("long serialVersionUID");
+    }
+
+    // -----------------------------------------------------
+    //                              Override Annotation Line
+    //                              ------------------------
     protected boolean isOverrideAnnotationLine(String line) {
         return line != null && line.trim().startsWith("@Override");
     }
 
+    // -----------------------------------------------------
+    //                                           Method Line
+    //                                           -----------
     protected boolean isMethodLine(String line) {
         final String trimmed = line.trim();
         final boolean indent = line.length() > trimmed.length();
         return indent && trimmed.startsWith("p") && trimmed.contains("(") && trimmed.endsWith(") {");
     }
 
+    // -----------------------------------------------------
+    //                                          Foreach Line
+    //                                          ------------
     protected boolean isForeachLine(String line) {
         return line != null && line.trim().startsWith("for ") && line.contains(" : ");
     }
@@ -112,6 +153,9 @@ public class CSharpClassContentsBuilder extends CSharpBuilderBase {
         return replace(replace(work, "for ", "foreach "), " : ", " in ");
     }
 
+    // -----------------------------------------------------
+    //                                          Various Line
+    //                                          ------------
     protected String doConvertInstanceOf(String work) {
         if (work != null && work.contains(" instanceof ")) {
             return replace(work, " instanceof ", " is ");
@@ -127,5 +171,12 @@ public class CSharpClassContentsBuilder extends CSharpBuilderBase {
             return null;
         }
         return replaceBy(work, _basicClassElementReplaceMap);
+    }
+
+    // -----------------------------------------------------
+    //                                        Filter Finally
+    //                                        --------------
+    protected String filterFinally(StringBuilder contentsSb) {
+        return Srl.replace(contentsSb.toString(), ln() + ln() + ln(), ln() + ln());
     }
 }
