bcfc68c9da5b37d2411fb50038818c71ee3d47c6
Kei Kotani <keikotani926@gmail.com>
2014-04-27 00:55:12 +0900

    Add [AbstratSet], [HashSet]

---
+++ dbflute.net-runtime/DBFluteRuntime/JavaLike/Util/HashSet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBFluteRuntime.JavaLike.Util
+{
+    // TODO 未実装
+    [Serializable]
+    public class HashSet<E> : AbstractSet<E>, ICloneable
+    {
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
