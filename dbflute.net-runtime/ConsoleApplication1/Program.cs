59236a6015e15591fc2652869468a00cac1aa6a7
Kei Kotani <keikotani926@gmail.com>
2014-04-27 14:58:37 +0900

    javaのbooleanとC#の関連付けを行う定義を追加

---
+++ dbflute.net-runtime/ConsoleApplication1/Program.cs
@@ -0,0 +1,70 @@
+﻿using DBFluteRuntime.JavaLike.Lang;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            boolean hoge1 = true;
+            boolean hoge2 = false;
+            if (hoge1)
+            {
+                Console.WriteLine("true");
+            }
+
+            if (hoge2)
+            {
+                Console.WriteLine("XXXXXXXXXXXX");
+            }
+
+            if (hoge1 && hoge2)
+            {
+                Console.WriteLine("hoge1 && hoge2 -> true");
+            }
+            if (hoge1 || hoge2)
+            {
+                Console.WriteLine("hoge1 || hoge2 -> true");
+            }
+
+            methodBool(hoge1);
+            methodBool(hoge2);
+
+            methodBoolean(true);
+            methodBoolean(false);
+
+            bool huga1 = hoge1;
+            bool huga2 = hoge2;
+
+            Console.WriteLine(retBool(true));
+            Console.WriteLine(retBoolean(new boolean(false)));
+            Thread.Sleep(2000);
+        }
+
+        private static void methodBool(bool b)
+        {
+            Console.WriteLine("methodTrue->{0}", b);
+        }
+
+        private static void methodBoolean(boolean b)
+        {
+            Console.WriteLine("methodTrue->{0}", b);
+        }
+
+        private static bool retBool(boolean b)
+        {
+            return b;
+        }
+
+        private static bool retBoolean(bool b)
+        {
+            return b;
+        }
+    }
+}
---
+++ dbflute.net-runtime/DBFluteRuntime/Test.cs
@@ -0,0 +1,21 @@
+﻿using DBFluteRuntime.JavaLike.Lang;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBFluteRuntime
+{
+    public class Test
+    {
+        public void hoge()
+        {
+            boolean b = new boolean(true);
+            if (b)
+            {
+                Console.WriteLine("Success");
+            }
+        }
+    }
+}
