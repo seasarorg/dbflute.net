d9c6c362769fb7f3c90f700d6108838827941519
Kei Kotani <keikotani926@gmail.com>
2014-04-27 13:28:45 +0900

    ObjectクラスのJava拡張追加

---
+++ dbflute.net-runtime/DBFluteRuntime/JavaLike/Lang/ObjectExtension.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace DBFluteRuntime.JavaLike.Lang
+{
+    // #pending getClassメソッドの対応
+    /// <summary>
+    /// java.lang.Objectの拡張クラス
+    /// </summary>
+    public static class ObjectExtension
+    {
+        /// <summary>
+        /// java風toStringメソッド定義
+        /// </summary>
+        /// <param name="o"></param>
+        /// <returns></returns>
+        public static string toString(this Object o)
+        {
+            return o.ToString();
+        }
+
+        /// <summary>
+        /// java風hashCodeメソッド定義
+        /// </summary>
+        /// <param name="o"></param>
+        /// <returns></returns>
+        public static int hashCode(this Object o)
+        {
+            return o.GetHashCode();
+        }
+
+        /// <summary>
+        /// java風equalsメソッド定義
+        /// </summary>
+        /// <param name="o"></param>
+        /// <returns></returns>
+        public static bool equals(this Object original, Object target)
+        {
+            return original.Equals(target);
+        }
+    }
+}
