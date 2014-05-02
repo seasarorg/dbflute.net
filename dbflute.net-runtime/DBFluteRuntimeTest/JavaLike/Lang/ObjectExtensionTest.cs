b7fa37defe938b01d25a874c37f5773b095d0298
Kei Kotani <keikotani926@gmail.com>
2014-05-03 00:39:40 +0900

    ObjectExtensionテストを追加

---
+++ dbflute.net-runtime/DBFluteRuntimeTest/JavaLike/Lang/ObjectExtensionTest.cs
@@ -0,0 +1,44 @@
+﻿using DBFluteRuntime.JavaLike.Lang;
+using NUnit.Framework;
+
+namespace DBFluteRuntimeTest.JavaLike.Lang
+{
+    /// <summary>
+    /// ObjectExtensionテスト
+    /// </summary>
+    [TestFixture]
+    public class ObjectExtensionTest
+    {
+        [Test]
+        public void TestToString()
+        {
+            object actual = "dbflute";
+            Assert.AreEqual(actual.ToString(), actual.toString());
+        }
+
+        [Test]
+        public void TestGetClass()
+        {
+            object actual = "dbflute";
+            Assert.AreEqual(actual.GetType(), actual.getClass());
+        }
+
+        [Test]
+        public void TestHashCode()
+        {
+            object actual = "dbflute";
+            Assert.AreEqual(actual.GetHashCode(), actual.hashCode());
+        }
+
+        [Test]
+        public void TestEquals()
+        {
+            object actualA = "dbflute";
+            object actualB = "dbflute";
+            object actualX = "runtime";
+
+            Assert.AreEqual(actualA.Equals(actualB), actualA.equals(actualB));
+            Assert.AreEqual(actualA.Equals(actualX), actualA.equals(actualX));
+        }
+    }
+}
