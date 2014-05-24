0353e4e7c65064a9e4abdd2694fb43fd7ce40974
Kei Kotani <keikotani926@gmail.com>
2014-05-24 13:00:14 +0900

    Connection, MetaData追加

---
+++ dbflute.net-runtime/DBFluteRuntime/JavaLike/Sql/DatabaseMetaData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBFluteRuntime.JavaLike.Sql
+{
+    // #pending 他クラスの実装の都合上、クラスだけ作成
+    /// <summary>
+    /// [Java]DatabaseMetaData
+    /// </summary>
+    public class DatabaseMetaData
+    {
+    }
+}
